d0a5f59b8396f2e76c97e4bb6228935f9ce767ff
BaczkiewiczDawid <baczkiewicz.dawid22@gmail.com>
2022-05-30 14:20:32 +0200

    fix server closing error

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const bodyParser = require("body-parser");
 
 require("dotenv").config();
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host: process.env.DB_SERVER,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
@@ -16,8 +16,6 @@ const db = mysql.createConnection({
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-db.connect();
 
 app.get("/api/posts", (req, res) => {
   const getPosts = "SELECT * FROM space_posts ORDER BY id DESC";
